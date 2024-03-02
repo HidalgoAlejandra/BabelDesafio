4a28f88853706920a735c6dc1a52d3d423b88f23
Alejandra <informaticotrabajo00@gmail.com>
2024-03-01 21:48:54 -0300

    validaciones

--- src/impuestos.js
+++ src/impuestos.js
@@ -1,28 +1,22 @@
 class Impuesto {
   constructor(montoBrutoAnual, deducciones) {
-    if (montoBrutoAnual >= 0) {
-      this._montoBrutoAnual = montoBrutoAnual;
-    } else {
-      this._montoBrutoAnual = null;
-    }
-
-    if (deducciones >= 0) {
-      this._deducciones = deducciones;
-    } else {
-      this._deducciones = null;
-    }
-
+    this._montoBrutoAnual = montoBrutoAnual;
+    this._deducciones = deducciones;
   }
+
   getMontoBrutoAnual() {
     return this._montoBrutoAnual;
   }
+
   setMontoBrutoAnual(montoBrutoAnual) {
     return (this._montoBrutoAnual = montoBrutoAnual);
   }
+
   getDeducciones() {
     return this._deducciones;
   }
-  setMontoBrutoAnual(deducciones) {
+
+  setDeducciones(deducciones) {
     return (this._deducciones = deducciones);
   }
 }
